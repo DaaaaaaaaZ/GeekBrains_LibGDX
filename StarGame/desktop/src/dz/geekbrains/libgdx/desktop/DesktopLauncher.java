aa173dc8dc1252ab2428b04d5608fd1714b7f298
DZ <msb_@bk.ru>
2021-08-28 05:11:45 +0300

    add lesson 1

--- StarGame/desktop/src/dz/geekbrains/libgdx/desktop/DesktopLauncher.java
+++ StarGame/desktop/src/dz/geekbrains/libgdx/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import dz.geekbrains.libgdx.StarGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 600;
+		config.width = 400;
+		config.resizable = false;
+		config.title = "Lesson 1";
 		new LwjglApplication(new StarGame(), config);
 	}
 }
